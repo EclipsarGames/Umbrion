9e8604997a3bc4c0d17f139d3e92c09f1c674203
EclipsarGames <134024358+EclipsarGames@users.noreply.github.com>
2024-05-30 13:43:16 -0500

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,7 +1,6 @@
 function getRandomGreeting() {
   var greetings = [
     "Beep boop! Are you a robot?",
-    "Did you know I have a discord server?",
     "How many sides does a circle have?",
     "You can read btw",
     "Made you look!",
