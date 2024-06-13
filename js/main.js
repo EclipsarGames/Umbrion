7019b9f496ded9db05028a147ad195b7ae6514cc
EclipsarGames <134024358+EclipsarGames@users.noreply.github.com>
2024-06-13 11:41:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.warn(
   "%cHello!!",
   "color: red; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "Feel free to use anything you find here for your projects; credit is appreciated but not required! Visit my website at https://3kh0.net for more information."
+  "Why are you here?"
 );
 
 // this setting controls if ads are shown,
